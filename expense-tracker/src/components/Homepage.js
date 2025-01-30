6276510549d02a87364e065d27718b970f275b2b
hey-raghav <raghavsharmanzre@gmail.com>
2025-01-30 14:09:26 +0530

    Register Form create

--- expense-tracker/src/components/Homepage.js
+++ expense-tracker/src/components/Homepage.js
@@ -5,12 +5,48 @@ function Homepage() {
     return (
         <div className='container'>
             <div className='leftcontainer'>
-
+                <p>Welcome to Expense Tracker</p>
+                <p>Manage all your expenses here</p>
                 <img src={img1} />
 
             </div>
             <div className='rightcontainer'>
-
+                <div className='formcard'>
+                    <div>
+                    <h2 style={{alignSelf:'center'}}>Register</h2>
+                    </div>
+                    <div>
+                        <label>
+                            Name
+                        </label>
+                        <input type="text"
+                            placeholder='Enter your name' />
+                    </div>
+                    <div>
+                        <label>
+                            Email
+                        </label>
+                        <input type="email"
+                            placeholder='Enter your email' />
+                    </div>
+                    <div>
+                        <label>
+                            Password
+                        </label>
+                        <input type="password"
+                            placeholder='Enter your password' />
+                    </div>
+                    <div>
+                        <label>
+                            Confirm Password
+                        </label>
+                        <input type="password"
+                            placeholder='Confirm your password' />
+                    </div>
+                    <div>
+                        <button>Submit</button>
+                    </div>
+                </div>
             </div>
 
         </div>
